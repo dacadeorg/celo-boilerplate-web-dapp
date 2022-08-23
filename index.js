5fe2bfbd7b960baa65365aec179550e7583e815e
Chr0n0sXIII <71567732+Chr0n0sXIII@users.noreply.github.com>
2022-08-23 10:48:05 -0400

    new project

--- index.js
+++ index.js
@@ -18,3 +18,5 @@ let app = new webpackDevServer(webpackCompiler, webpackDevServerOptions);
 
 let port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`App listening on ${port}`));
+
+
--- src/main.js
+++ src/main.js
@@ -209,3 +209,133 @@ document.querySelector("#marketplace").addEventListener("click", async (e) => {
     }
   }
 })  
+
+(function ($) {
+  "use strict";
+  
+  // Dropdown on mouse hover
+  $(document).ready(function () {
+      function toggleNavbarMethod() {
+          if ($(window).width() > 992) {
+              $('.navbar .dropdown').on('mouseover', function () {
+                  $('.dropdown-toggle', this).trigger('click');
+              }).on('mouseout', function () {
+                  $('.dropdown-toggle', this).trigger('click').blur();
+              });
+          } else {
+              $('.navbar .dropdown').off('mouseover').off('mouseout');
+          }
+      }
+      toggleNavbarMethod();
+      $(window).resize(toggleNavbarMethod);
+  });
+
+
+  // Date and time picker
+  $('.date').datetimepicker({
+      format: 'L'
+  });
+  $('.time').datetimepicker({
+      format: 'LT'
+  });
+  
+  
+  // Back to top button
+  $(window).scroll(function () {
+      if ($(this).scrollTop() > 100) {
+          $('.back-to-top').fadeIn('slow');
+      } else {
+          $('.back-to-top').fadeOut('slow');
+      }
+  });
+  $('.back-to-top').click(function () {
+      $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+      return false;
+  });
+
+
+  // Team carousel
+  $(".team-carousel, .related-carousel").owlCarousel({
+      autoplay: true,
+      smartSpeed: 1000,
+      center: true,
+      margin: 30,
+      dots: false,
+      loop: true,
+      nav : true,
+      navText : [
+          '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+          '<i class="fa fa-angle-right" aria-hidden="true"></i>'
+      ],
+      responsive: {
+          0:{
+              items:1
+          },
+          576:{
+              items:1
+          },
+          768:{
+              items:2
+          },
+          992:{
+              items:3
+          }
+      }
+  });
+
+
+  // Testimonials carousel
+  $(".testimonial-carousel").owlCarousel({
+      autoplay: true,
+      smartSpeed: 1500,
+      margin: 30,
+      dots: true,
+      loop: true,
+      center: true,
+      responsive: {
+          0:{
+              items:1
+          },
+          576:{
+              items:1
+          },
+          768:{
+              items:2
+          },
+          992:{
+              items:3
+          }
+      }
+  });
+
+
+  // Vendor carousel
+  $('.vendor-carousel').owlCarousel({
+      loop: true,
+      margin: 30,
+      dots: true,
+      loop: true,
+      center: true,
+      autoplay: true,
+      smartSpeed: 1000,
+      responsive: {
+          0:{
+              items:2
+          },
+          576:{
+              items:3
+          },
+          768:{
+              items:4
+          },
+          992:{
+              items:5
+          },
+          1200:{
+              items:6
+          }
+      }
+  });
+  
+})(jQuery);
+
